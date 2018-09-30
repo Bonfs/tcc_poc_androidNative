6c142a6727686f352a391cc850bdeb148f7c6437
bonfs <matheusbonfim05@gmail.com>
2018-09-30 20:29:32 -0300

    adiçao de mapa

--- app/src/main/java/com/pocnative/bonfim/pocnativeandroid/activity/ActivityFragment.kt
+++ app/src/main/java/com/pocnative/bonfim/pocnativeandroid/activity/ActivityFragment.kt
@@ -6,14 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import com.pocnative.bonfim.pocnativeandroid.R
-class ActivityFragment : androidx.fragment.app.Fragment() {
+class ActivityFragment : androidx.fragment.app.Fragment(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        val view = inflater.inflate(R.layout.fragment_activity, container, false)
+        val mapFragment: SupportMapFragment = childFragmentManager?.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return view
     }
 
+    override fun onMapReady(map: GoogleMap?) {
+        if(map != null){
+            this.map = map
+
+            val sydney = LatLng(-34.0, 151.0)
+            this.map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            this.map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
+    }
 }
